60894ae1ad807f0c914af52dd364ae6b686c8b01
Kellan Childers <Kellan Childers@gmail.com>
2015-08-06 22:21:13 -0400

    Setup basic display for shopping list.

--- recapp.py
+++ recapp.py
@@ -6,8 +6,14 @@ from recipe import Recipe
 
 class ShoppingList(Widget):
     def __init__(self, **kwargs):
-        self.shopping_list = Recipe()
+        self.recipe = Recipe()
         super(ShoppingList, self).__init__(**kwargs)
+
+    def list_items(self):
+        return_string = ""
+        for item in self.recipe:
+            return_string = return_string + '\n' + self.recipe.show_ingredient(item)
+        return return_string
 
 
 class MainScreen(Widget):
