756a895a4be06fe414a05390c27fb017ffb6024c
Kellan Childers <Kellan Childers@gmail.com>
2015-08-09 11:35:45 -0400

    Added comments to user interface for clarity.

--- userinterface/mainscreen.py
+++ userinterface/mainscreen.py
@@ -1,8 +1,11 @@
 __author__ = "Kellan Childers"
 from kivy.uix.widget import Widget
 from kivy.lang import Builder
+
+# Load the kvlang file associated with this file.
 Builder.load_file('userinterface/mainscreen.kv')
 
 
 class MainScreen(Widget):
+    """Placeholder to assist formatting of the main screen."""
     pass
--- userinterface/menus.py
+++ userinterface/menus.py
@@ -2,20 +2,26 @@ __author__ = 'Kellan Childers'
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.button import Button
 from kivy.lang import Builder
+
+# Load the kvlang file associated with this file.
 Builder.load_file('userinterface/menus.kv')
 
 
 class MenuBar(BoxLayout):
+    """Placeholder to assist formatting of all menus in application."""
     pass
 
 
 class TopMenu(MenuBar):
+    """Placeholder to assist formatting top menu bar."""
     pass
 
 
 class BottomMenu(MenuBar):
+    """Placeholder to assist formatting bottom menu bar."""
     pass
 
 
 class SimpleButton(Button):
+    """Placeholder to assist formatting all buttons in application."""
     pass
